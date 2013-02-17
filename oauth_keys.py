ef5499a09b5599b08fc4fd347f20ed1fef3287d3
Robert Marchman <robert.l.marchman@dartmouth.edu>
2013-02-17 16:39:32 +1300

    removing keys...

--- oauth_keys.py
+++
@@ -1,9 +0,0 @@
-
-# Move this file to oauth_keys.py, and copy in your 
-# HathiTrust OAuth keys. 
-# Credentials can be acquired from KGS here:
-# http://babel.hathitrust.org/cgi/kgs/request
-
-client_key = u'913deed90a'
-client_secret= u'd59b577ae558fe3e36f339603729'
-
