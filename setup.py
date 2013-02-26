a4ddc41bb499eb1711d4408eb3f9eae8557c3688
Robert Marchman <robert.l.marchman@dartmouth.edu>
2013-02-26 14:12:55 +1300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hathitrust_api', 
+      url='github.com/rlmv/hathitrust-api',
+      description='Python wrappers for the HathiTrust APIs',
+      author='Robert Marchman', 
+      packages=['hathitrust_api']
+      )
