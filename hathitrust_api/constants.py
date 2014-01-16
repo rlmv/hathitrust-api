b78ebcf96f03dafa54980bb6ef89b5bb99082bdc
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-01-16 16:27:18 -0500

    fix solr query stub

--- hathitrust_api/constants.py
+++ hathitrust_api/constants.py
@@ -13,7 +13,7 @@ SECURE_DATA_BASEURL = ''.join(['https://', _STUB])
 # constants for solrproxy
 _SOLR_HOST = "http://chinkapin.pti.indiana.edu"
 _SOLR_PORT = 9994
-_QUERY_STUB = "/solr/select/"
+_QUERY_STUB = "/solr/meta/select/"
 _MARC_STUB = "/solr/MARC/"
 
 QUERY_BASEURL = ''.join([_SOLR_HOST, ':', str(_SOLR_PORT), _QUERY_STUB])
