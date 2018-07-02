150e1b0be17ff05d1391e90d55282009c33953ec
Bo Marchman <bo.marchman@gmail.com>
2018-07-02 11:46:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
+with open('README.md') as f:
+    long_description = f.read()
 
-setup(name='hathitrust_api', 
-      url='github.com/rlmv/hathitrust-api',
-      description='Python wrappers for the HathiTrust APIs',
-      author='Robert Marchman', 
-      packages=['hathitrust_api'],
-      install_requires=['requests', 'requests-oauthlib']
-      )
+setuptools.setup(
+    name='hathitrust_api',
+    version='0.1.0',
+    author='Bo Marchman',
+    url='https://github.com/rlmv/hathitrust-api',
+    description='Python wrappers for the HathiTrust APIs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['hathitrust_api'],
+    install_requires=[
+        'requests',
+        'requests-oauthlib'
+    ],
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
